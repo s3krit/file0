89ad99159a819ecd2b81514abb667f15c818a9f7
Martin Pugh <pugh@s3kr.it>
2018-01-28 14:29:30 +0100

    Add multi-file upload, and some neatening
    Multi-file upload seems fairly simple and effective. Also took this
    opportunity to neaten up some of the rendering of the successful upload
    page - notably, less static strings and more use of the request object.

--- app/routes/posts.rb
+++ app/routes/posts.rb
@@ -9,18 +9,33 @@ module File0
 
       post '/upload' do
         # Send shit to create_file
-        file = params['file']
-        unless file
+        files = params['files']
+        unless files
           status 400
-          return render_generic("No file selected","Helps if you select a file, mate")
+          return render_generic("No files selected","Helps if you select a file, mate")
         end
-        uploaded_file = File0::File.create(file[:tempfile],file[:type], session[:key])
+
+        file_urls = []
+        files.each do |file|
+          file_urls.push File0::File.create(file[:tempfile],file[:type], session[:key])
+        end
+
         # Redirect to uploaded file if we get a url, else die?
-        unless uploaded_file
-          #status 400
+        if file_urls.empty?
+          status 500
           return render_generic("Oops!","Something went wrong")
         end
-        render_generic("File uploaded","Take your file mate, it won't be here forever. <a href=#{uploaded_file}>https://#{request.host+"/"+uploaded_file}</a>")
+        if request.port == 80 then host_string = request.host else host_string = request.host_with_port end
+        if file_urls.size == 1
+          return render_generic("File uploaded","Take your file mate, it won't be here forever. <a href=#{file_urls.first}>#{request.scheme}://#{host_string+"/"+file_urls.first}</a>")
+        else
+          body_str = "Take your files mate, they won't be here forever.\n<ul>\n"
+          file_urls.each do |url|
+            body_str += "<li><a href=#{url}>#{request.scheme}://#{host_string+"/"+url}</a></li>\n"
+          end
+          body_str += "</ul>\n"
+          return render_generic("Files uploaded", body_str)
+        end
       end
     end
   end
