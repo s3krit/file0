2f7815ad75d6169eeecf1899c8a3a6b7181a4e2a
Martin Pugh <pugh@s3kr.it>
2018-02-04 20:05:09 +0100

    Add 'copy to clipboard' button and make uploaded page use a proper view

--- app/routes/posts.rb
+++ app/routes/posts.rb
@@ -8,34 +8,37 @@ module File0
       end
 
       post '/upload' do
-        # Send shit to create_file
+
         files = params['files']
         unless files
           status 400
           return render_generic("No files selected","Helps if you select a file, mate")
         end
 
-        file_urls = []
+        @file_urls = []
         files.each do |file|
-          file_urls.push File0::File.create(file[:tempfile],file[:type], session[:key])
+          @file_urls.push File0::File.create(file[:tempfile],file[:type], session[:key])
         end
 
         # Redirect to uploaded file if we get a url, else die?
-        if file_urls.empty?
+        if @file_urls.empty?
           status 500
           return render_generic("Oops!","Something went wrong")
         end
-        if request.port == 80 then host_string = request.host else host_string = request.host_with_port end
-        if file_urls.size == 1
-          return render_generic("File uploaded","Take your file mate, it won't be here forever. <a href=#{file_urls.first}>#{request.scheme}://#{host_string+"/"+file_urls.first}</a>")
+        if request.port == 80 or request.port == 443
+          @host_string = request.host
         else
-          body_str = "Take your files mate, they won't be here forever.\n<ul>\n"
-          file_urls.each do |url|
-            body_str += "<li><a href=#{url}>#{request.scheme}://#{host_string+"/"+url}</a></li>\n"
-          end
-          body_str += "</ul>\n"
-          return render_generic("Files uploaded", body_str)
+          @host_string = request.host_with_port
         end
+
+        if request.secure?
+          @host_string = "https://" + @host_string
+        else
+          @host_string = "http://" + @host_string
+        end
+
+        @pagetype = :uploaded
+        return erb :base
       end
     end
   end
