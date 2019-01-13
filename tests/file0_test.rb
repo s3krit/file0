faece81ca0db27064745632ee7a4564d5d626d29
Martin Pugh <pugh@s3kr.it>
2019-01-13 22:17:17 +0100

    Fix tests, add new tests, remove unused dropzone.css

--- tests/file0_test.rb
+++ tests/file0_test.rb
@@ -11,7 +11,9 @@ require 'base64'
 class File0Test < Test::Unit::TestCase
   include Rack::Test::Methods
   redis_host = ENV['REDIS_PORT_6379_TCP_ADDR'] || 'localhost'
-  $redis = Redis.new(host: redis_host)
+  redis = Redis.new(host: redis_host)
+  $file_redis = Redis::Namespace.new(:file, redis: redis)
+  $album_redis = Redis::Namespace.new(:album, redis: redis)
 
   def app
     File0::App.new
@@ -35,21 +37,36 @@ class File0Test < Test::Unit::TestCase
     assert_equal 404, last_response.status
   end
 
+  def test_it_receives_uploaded_files
+    file_count = File0::File.all.size
+    post('/upload', "files[]" => Rack::Test::UploadedFile.new('tests/testfile.txt'))
+    assert last_response.body.include? 'Take your file'
+    assert_equal File0::File.all.size, file_count + 1
+  end
+
+  def test_it_receives_multiple_uploaded_files
+    file_count = File0::File.all.size
+    f = Rack::Test::UploadedFile.new('tests/testfile.txt')
+    post('/upload', {files: [f,f]})
+    assert last_response.body.include? 'Take your files'
+    assert_equal File0::File.all.size, file_count + 2
+  end
+
   def test_it_retrieves_valid_files
     filename = 'bbbbbbbbbbbb.jpg'
     payload = {
       filetype: 'text/plain',
       data: Base64.encode64("Hello, world!\n").strip
     }
-    $redis.set(filename, payload.to_json)
+    $file_redis.set(filename, payload.to_json)
     get '/bbbbbbbbbbbb.jpg'
     assert last_response.body.include? 'Hello, world'
   end
 
   def test_it_throws_404s_of_expired_files
     filename = 'bbbbbbbbbbbb.jpg'
-    assert $redis.get(filename)
-    $redis.expire(filename, 1)
+    assert $file_redis.get(filename)
+    $file_redis.expire(filename, 1)
     sleep 2
     get '/bbbbbbbbbbbb.jpg'
     assert_equal 404, last_response.status
@@ -68,19 +85,19 @@ class File0Test < Test::Unit::TestCase
       data: Base64.encode64("Hello, world!\n").strip,
       gallery: 'on'
     }
-    $redis.set(filename, payload.to_json)
+    $file_redis.set(filename, payload.to_json)
     get '/gallery'
     assert last_response.body.include?('bbbbbbbbbbbb.jpg')
   end
 
-  def test_gallery_page_does_list_unapproved_files
+  def test_gallery_page_doesnt_list_unapproved_files
     filename = 'abcdefgh.jpg'
     payload = {
       filetype: 'text/plain',
       data: Base64.encode64("Hello, world!\n").strip,
       gallery: nil
     }
-    $redis.set(filename, payload.to_json)
+    $file_redis.set(filename, payload.to_json)
     get '/gallery'
     assert !last_response.body.include?('abcdefgh.jpg')
   end
