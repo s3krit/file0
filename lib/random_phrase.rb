01e3d65df64bcd9a73d6bb610d3f96248000e903
rasbary <rasbary432@gmail.com>
2020-05-15 14:47:55 -0500

    Add RandomPhrase for human readable url generation

---
+++ lib/random_phrase.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'securerandom'
+require_relative 'word_bank'
+
+module File0
+  class RandomPhrase
+    DEFAULT_OPTIONS = {
+      adjectives: 2,
+      nouns: 1
+    }
+
+    def self.generate(options = {})
+      options.merge!(DEFAULT_OPTIONS)
+
+      ''.dup.tap do |phrase|
+        options.each do |word_type, count|
+          count.times do
+            const_name = word_type.to_s.upcase
+            available_words = File0::WordBank.const_get(const_name).length
+            selection = SecureRandom.random_number(available_words)
+            phrase << File0::WordBank.const_get(const_name)[selection].to_s.downcase.capitalize
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/test/random_phrase_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require_relative '../random_phrase'
+
+class RandomPhraseTest < Test::Unit::TestCase
+  def test_unique_phrase_generation
+    phrases = (1..10000).map { File0::RandomPhrase.generate }
+    assert_equal 10000, phrases.uniq.count
+  end
+end
